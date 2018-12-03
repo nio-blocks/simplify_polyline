91e3112e268bc76c0c063c85ccaed733e2f5f19f
Tom Young <tyoung@n.io>
2018-12-03 12:15:05 -0700

    create unit test

--- tests/test_simplify_polyline_block.py
+++ tests/test_simplify_polyline_block.py
@@ -1,19 +1,39 @@
 from nio.block.terminals import DEFAULT_TERMINAL
-from nio.signal.base import Signal
+from nio import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
 from ..simplify_polyline_block import SimplifyPolyline
+from .test_values import test_points, test_results
 
 
 class TestSimplifyPolyline(NIOBlockTestCase):
+    """
+    source: https://github.com/mourner/simplify-js/blob/master/test/test.js
+    """
 
-    def test_process_signals(self):
-        """Signals pass through block unmodified."""
+    signal_list = [Signal(point) for point in test_points]
+    maxDiff = None
+
+    def _get_result_signals(self, group=None):
+        result_signals = []
+        for result in test_results:
+            result['group'] = group
+            result_signals.append(Signal(result))
+        return result_signals
+
+    def test_simplify(self):
+        """Simplifies points correctly with the given tolerance."""
         blk = SimplifyPolyline()
-        self.configure_block(blk, {})
+        self.configure_block(blk, {'tolerance': 5})
         blk.start()
-        blk.process_signals([Signal({"hello": "nio"})])
+        blk.process_signals(self.signal_list)
         blk.stop()
-        self.assert_num_signals_notified(1)
-        self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "nio"})
+        self.assert_last_signal_list_notified(self._get_result_signals())
+
+    def test_one_point(self):
+        """Just return the points if it has only one point."""
+        blk = SimplifyPolyline()
+        self.configure_block(blk, {'tolerance': 5})
+        blk.start()
+        blk.process_signals([self.signal_list[0]])
+        blk.stop()
+        self.assert_last_signal_list_notified([self._get_result_signals()[0]])
