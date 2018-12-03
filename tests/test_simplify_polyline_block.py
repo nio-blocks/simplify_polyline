7dfd69ef552746f512da7cd609c06efdf5e8a5a8
Tom Young <tyoung@n.io>
2018-12-03 12:25:00 -0700

    fix bad assertion in unit test

--- tests/test_simplify_polyline_block.py
+++ tests/test_simplify_polyline_block.py
@@ -10,11 +10,9 @@ class TestSimplifyPolyline(NIOBlockTestCase):
     source: https://github.com/mourner/simplify-js/blob/master/test/test.js
     """
 
-    signal_list = [Signal(point) for point in test_points]
-
-    def _get_result_signals(self, group=None):
+    def _get_result_signals(self, results, group=None):
         result_signals = []
-        for result in test_results:
+        for result in results:
             result['group'] = group
             result_signals.append(Signal(result))
         return result_signals
@@ -24,15 +22,17 @@ class TestSimplifyPolyline(NIOBlockTestCase):
         blk = SimplifyPolyline()
         self.configure_block(blk, {'tolerance': 5})
         blk.start()
-        blk.process_signals(self.signal_list)
+        blk.process_signals([Signal(point) for point in test_points])
         blk.stop()
-        self.assert_last_signal_list_notified(self._get_result_signals())
+        self.assert_last_signal_list_notified(
+            self._get_result_signals(test_results))
 
     def test_one_point(self):
         """Just return the points if it has only one point."""
         blk = SimplifyPolyline()
         self.configure_block(blk, {'tolerance': 5})
         blk.start()
-        blk.process_signals([self.signal_list[0]])
+        blk.process_signals([Signal(test_points[0])])
         blk.stop()
-        self.assert_last_signal_list_notified([self._get_result_signals()[0]])
+        self.assert_last_signal_list_notified(
+            self._get_result_signals([test_points[0]]))
